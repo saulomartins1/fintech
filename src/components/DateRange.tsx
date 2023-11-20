b1c07b0a858ec45389cad77d52712fbe974b22c3
d3Niro <saulomartins2020@gmail.com>
2023-11-20 12:31:01 -0300

    feat: reactive Data fetch

--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
+import { useData } from '../context/DataContext';
 import DateInput from './DateInput'
 
 function DateRange() {
-    const [inicio, setInicio] = React.useState('');
-    const [final, setFinal] = React.useState('');
+    const { inicio, setInicio, final, setFinal } = useData();
 
     return (
         <form className='box flex' onSubmit={(e) => e.preventDefault()}>
             <DateInput id={'inicio'} label={"Início"} value={inicio}
                 onChange={({ target }) => setInicio(target.value)}
             />
-            {inicio}
             <DateInput id={'final'} label={"Final"} value={final}
                 onChange={({ target }) => setFinal(target.value)}
             />
-            {final}
         </form>
     )
 }
