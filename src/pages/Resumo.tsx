7708e1bf2d64097d820182539662f4cc5f57409e
d3Niro <saulomartins2020@gmail.com>
2023-11-21 18:47:49 -0300

    feat: sales summary (sales, received, pending) amount

--- src/pages/Resumo.tsx
+++ src/pages/Resumo.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useData } from '../context/DataContext';
 
-function resumo() {
+function Resumo() {
+    const { data } = useData();
+
+    if (!data) return null;
     return (
-        <div>Resumo</div>
+        <section>
+            <div className='resumo flex mb'>
+                <div className='box'>
+                    <h2>Vendas Total</h2>
+                    <span>
+                        {data.filter((v) => v.status !== 'falha')
+                            .reduce((acc, preco) => {
+                                return acc + preco.preco;
+                            }, 0).toLocaleString('pt-BR', { style: 'currency', currency: "BRL" })}
+                    </span>
+                </div>
+                <div className='box'>
+                    <h2>Recebido</h2>
+                    <span>
+                        {data.filter((v) => v.status === 'pago')
+                            .reduce((acc, preco) => {
+                                return acc + preco.preco;
+                            }, 0).toLocaleString('pt-BR', { style: 'currency', currency: "BRL" })}
+                    </span>
+                </div>
+                <div className='box'>
+                    <h2>Vendas</h2>
+                    <span>
+                        {data.filter((v) => v.status === 'processando')
+                            .reduce((acc, preco) => {
+                                return acc + preco.preco;
+                            }, 0).toLocaleString('pt-BR', { style: 'currency', currency: "BRL" })}
+                    </span>
+                </div>
+            </div>
+        </section>
     )
 }
 
-export default resumo
+export default Resumo
