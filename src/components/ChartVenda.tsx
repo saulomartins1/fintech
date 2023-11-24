031489462f757a1abd8c5a369e95fdfab8073e8c
d3Niro <saulomartins2020@gmail.com>
2023-11-24 10:36:23 -0300

    feat: rechart sales data infos by days

--- src/components/ChartVenda.tsx
+++ src/components/ChartVenda.tsx
@@ -2,39 +2,49 @@ import React from 'react'
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import { IVenda } from '../context/DataContext'
 
-const dadosChart = [
-    {
-        data: '2023-11-03',
-        pago: 600,
-        processando: 300,
-        falha: 100,
-    },
-    {
-        data: '2023-11-04',
-        pago: 6001,
-        processando: 3001,
-        falha: 1001,
-    },
-    {
-        data: '2023-11-05',
-        pago: 3000,
-        processando: 1000,
-        falha: 200,
-    },
-]
+
+type IVendaDia = {
+    data: string,
+    pago: number,
+    processando: number,
+    falha: number,
+
+}
+
+function transformData(data: IVenda[]): IVendaDia[] {
+
+    const dias = data.reduce((acc: { [key: string]: IVendaDia }, item) => {
+        const dia = item.data.split(' ')[0];
+        if (!acc[dia]) {
+            acc[dia] = {
+                data: dia.substring(5),
+                pago: 0,
+                falha: 0,
+                processando: 0,
+            };
+        }
+        acc[dia][item.status] = + item.preco;
+        return acc;
+    }, {})
+
+
+    return Object.values(dias);
+}
 
 function ChartVenda({ data }: { data: IVenda[] }) {
+    const transformedData = transformData(data);
+
     return (
         <ResponsiveContainer width={"99%"} height={400}>
-            <LineChart data={dadosChart}>
+            <LineChart data={transformedData}>
                 <XAxis dataKey="data" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
                 <CartesianGrid stroke="#f5f5f5" />
-                <Line type="monotone" dataKey="pago" stroke="#3363FF" strokeWidth={3} />
-                <Line type="monotone" dataKey="processando" stroke="#FBCB21" strokeWidth={3} />
-                <Line type="monotone" dataKey="falha" stroke="#000" strokeWidth={3} />
+                <Line type="monotone" dataKey="pago" stroke="#3363FF" strokeWidth={2} />
+                <Line type="monotone" dataKey="processando" stroke="#FBCB21" strokeWidth={2} />
+                <Line type="monotone" dataKey="falha" stroke="#000" strokeWidth={2} />
             </LineChart>
         </ResponsiveContainer>
     )
