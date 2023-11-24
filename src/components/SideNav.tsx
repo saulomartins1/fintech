ec4be5e46da46f349c41b3ab61ed9e4cab4baeb5
d3Niro <saulomartins2020@gmail.com>
2023-11-24 10:56:11 -0300

    style: responsive | final adjustments

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -18,32 +18,22 @@ function SideNav() {
                     <span><img src={resumo} alt="" /></span>
                     <NavLink to="/">Resumo</NavLink>
                 </li>
-            </ul>
-            <ul>
                 <li>
                     <span><img src={vendas} alt="" /></span>
                     <NavLink to="/vendas">Vendas</NavLink>
                 </li>
-            </ul>
-            <ul>
                 <li>
                     <span><img src={webhooks} alt="" /></span>
                     <NavLink to="/">Webhooks</NavLink>
                 </li>
-            </ul>
-            <ul>
                 <li>
                     <span><img src={configs} alt="" /></span>
                     <NavLink to="/">Configurações</NavLink>
                 </li>
-            </ul>
-            <ul>
                 <li>
                     <span><img src={contato} alt="" /></span>
                     <NavLink to="/">Contato</NavLink>
                 </li>
-            </ul>
-            <ul>
                 <li>
                     <span><img src={sair} alt="" /></span>
                     <NavLink to="/">Sair</NavLink>
